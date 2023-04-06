8cea33d11181de963083d255142725147601f78c
JaroslawStepien <stepien.jarek0227@gmail.com>
2023-04-06 23:31:47 +0200

    handle exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  rescue_from ActiveRecord::RecordNotFound, with: :exception
+  rescue_from NoMethodError, with: :exception
+
+  def exception
+    redirect_to home_about_url, notice: "You do not have permission to perform this action"
+  end
+
 end
